46ea6ebc61ec07868993b625efe11775bb097ce2
DONOTINTO <goods99j@gmail.com>
2024-05-14 18:08:28 +0900

    Init: 프로젝트 생성 및 초기 세팅

---
+++ ToughCookie/ToughCookie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToughCookie
+//
+//  Created by 이중엽 on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
